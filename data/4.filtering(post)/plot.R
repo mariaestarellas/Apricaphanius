0a4d07de5a09dcfacf26f7ff9721fe8ed1120f2a
Maria Estarellas <122981372+mariaestarellas@users.noreply.github.com>
2023-12-05 16:09:31 +0100

    Update plot.R

--- data/4.filtering(post)/plot.R
+++ data/4.filtering(post)/plot.R
@@ -2,8 +2,10 @@ setwd ("~/DiscoC/aphanius/copy_data/filtered/haplotypecaller/genotypecaller/filt
 library(ggplot2)
 dataset <- read.delim("dataset3.table")
 y <- c("QD", "FS", "SOR", "MQ", "MQRankSum", "ReadPosRankSum")
-for (i in y){
-pdf(file= paste0(i,".pdf"))
-ggplot(dataset, aes(x= i)) + geom_density(color="darkblue", fill="lightblue")
-dev.off()
+
+for (i in y) {
+  pdf(file = paste0(i, ".pdf"))
+  print(ggplot(dataset, aes(x = get(i))) + 
+          geom_density(color = "darkblue", fill = "lightblue"))
+  dev.off()                                   
 }
