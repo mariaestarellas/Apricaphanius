30e7c833377bb11efb509e42ae5f0ac00c111cf7
Maria Estarellas <122981372+mariaestarellas@users.noreply.github.com>
2023-12-05 13:01:05 +0100

    Update plot.R

--- data/4.filtering(post)/plot.R
+++ data/4.filtering(post)/plot.R
@@ -3,7 +3,7 @@ library(ggplot2)
 dataset <- read.delim("dataset3.table")
 y <- c("QD", "FS", "SOR", "MQ", "MQRankSum", "ReadPosRankSum")
 for (i in y){
-pdf(file= paste0(i, ".pdf"))
+pdf(file= paste0(i,".pdf"))
 ggplot(dataset, aes(x= i)) + geom_density(color="darkblue", fill="lightblue")
 dev.off()
 }
