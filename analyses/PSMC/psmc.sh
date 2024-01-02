6f53f484c616c0aadcafb4d44b6f3211bdfbcb98
Maria Estarellas <122981372+mariaestarellas@users.noreply.github.com>
2024-01-02 14:48:04 +0100

    Update psmc.sh

--- analyses/PSMC/psmc.sh
+++ analyses/PSMC/psmc.sh
@@ -6,21 +6,21 @@ bcftools mpileup -C10 -f ~/DiscoC/aphanius/copy_data/reference_genome/assembly/A
 
 /home/panthera/software/psmc/utils/fq2psmcfa -q20 CN21596_diploid.fq.gz > CN21596_diploid.psmcfa 
 
-2) Now we can run psmc:
+#2) Now we can run psmc:
 
 psmc –N25 -t150 -r2 -p "4+25*2+4+6" -o CN21596_diploid.psmc CN21596_diploid.psmcfa
 
-3) Time to Plot.
+#3) Time to Plot.
 We need to take into consideration which is the generation time (-g) which in our case is 2 per year and the mutation rate (-u) which is 3.5x10-9 bp/generation. 
 
 softwares/psmc/utils/psmc_plot.pl -u 3.5e-9 -g 0.5 diploid diploid.psmc 
 
-4) Finally bootstrap (10)
+#4) Finally bootstrap (10)
 Firts, we need to split the psmcfa file into shorter fragments. 
 
 /home/panthera/software/psmc/utils/splitfa CN21596_diploid.psmcfa > CN21596_split.psmcfa
 
-Now do an iterative loop to do 10 rounds. Don't forget the -b flag!
+#Now do an iterative loop to do 10 rounds. Don't forget the -b flag!
 
 for i in $(seq 1 10);
 > do
