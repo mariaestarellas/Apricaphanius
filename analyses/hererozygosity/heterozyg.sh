975ff33ee768a27b08a1c337d4c5588d51013f7a
Maria Estarellas <122981372+mariaestarellas@users.noreply.github.com>
2024-02-09 17:56:11 +0100

    Update heterozyg.sh

--- analyses/hererozygosity/heterozyg.sh
+++ analyses/hererozygosity/heterozyg.sh
@@ -5,17 +5,17 @@ vcf="/home/goliath/DiscoC/aphanius/copy_data/filtered/haplotypecaller/genotypeca
 sample="dataset1.txt"
 window=100000
 
-rm "$sample"_heterozygosity.txt
 for sample in $(cat $sample);
 do
+	rm "$sample"_heterozygosity.txt
 	for region in $(cat complete_windows.txt);
 	do
 	callable=$(tabix -h $vcf $region | vcftools --gzvcf - --indv $sample --remove-indels --max-alleles 2 --minQ 30 --minDP 2 --maxDP 300 --stdout --recode --recode-INFO-all | grep -v '#' | wc -l ) 
 		if [ $callable -gt 0 ];
 		then
-			value=${region#*&}
-			start=$(echo "$value" |cut -d- -f1)
-			chrom=$(echo "$region" |cut -d& -f1)
+			value=${region#*.}
+			start=$(echo "$value" |cut -d; -f1)
+			chrom=$(echo "$region" |cut -d. -f1)
 			tabix -h $vcf $region | vcftools --gzvcf - --indv $sample --remove-indels --max-alleles 2 --minQ 30 --minDP 2 --maxDP 300 --stdout --recode --recode-INFO-all | vcflib vcfhetcount | tail -n 1 | awk -v l="$callable" -v chrom="$chrom" -v start="$start" -v window="$window" '{print chrom"\t"start+window"\t"$1"\t"l"\t"$1/l}' | tee -a "$sample"_heterozygosity.txt
 		else
 			echo $line | awk -v l=0 -v het=NA -v window="$window" '{print $1"\t"$2+window"\t"l"\t"l"\t"het}' | tee -a "$sample"_heterozygosity.txt
