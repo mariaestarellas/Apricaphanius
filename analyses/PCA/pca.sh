b5135832ef533e9e12267edc7c4384e28076b746
Maria Estarellas <122981372+mariaestarellas@users.noreply.github.com>
2024-02-12 10:52:52 +0100

    Update pca.sh

--- analyses/PCA/pca.sh
+++ analyses/PCA/pca.sh
@@ -1,5 +1,5 @@
-#First of all you need to create a fam, a bed and a bim file for your vcf:
-plink --vcf /home/goliath/DiscoC/aphanius/copy_data/filtered/haplotypecaller/genotypecaller/filtervcf/dataset2_filtered_usnps05.vcf.gz --allow-extra-chr --double-id --make-bed --out aph_iberus
+#First of all you need to create a fam, a bed and a bim file for your vcf (usnps):
+plink --vcf/home/goliath/DiscoC/aphanius/copy_data/filtered/haplotypecaller/genotypecaller/filtervcf/1.all_aphanius/dataset2_rmrep_gatk_nomissing_usnps05.vcf.gz --allow-extra-chr --double-id --make-bed --out aph_iberus_nomissing
 
 #With plink, you create the .eigenvec and .eigenval files that you will use for the PCA:
-plink --bfile aph_iberus --double-id --allow-extra-chr --set-missing-var-ids @:# --make-bed --pca --mind --out aph_iberus_pca
+plink --bfile aph_iberus_nomissing --double-id --allow-extra-chr --set-missing-var-ids @:# --make-bed --pca --mind --out aph_iberus_nomissing_pca
