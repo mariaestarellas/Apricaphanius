2a39c974c18900a62eed85e05cec35ce5d904cac
Maria Estarellas <122981372+mariaestarellas@users.noreply.github.com>
2024-04-10 10:24:11 +0200

    Update script1.py

--- analyses/ROH/script1.py
+++ analyses/ROH/script1.py
@@ -1,6 +1,6 @@
 bad_words = ["ST"]
 
-with open("roh_filtered.txt") as old_file, open("new_file.txt","w") as newfile:
+with open("roh.txt") as old_file, open("noST_roh.txt","w") as newfile:
  for line in old_file:
   if not any(bad_word in line for bad_word in bad_words):
    newfile.write(line)
