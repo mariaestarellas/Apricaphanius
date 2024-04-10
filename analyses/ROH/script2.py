002d7c95b1ca8ca877957d7b791be7b93bd04120
Maria Estarellas <122981372+mariaestarellas@users.noreply.github.com>
2024-04-10 10:25:41 +0200

    Update script2.py

--- analyses/ROH/script2.py
+++ analyses/ROH/script2.py
@@ -1,9 +1,9 @@
-for i in $(bcftools query -l dataset1_rohs.vcf.gz);
-do
-echo "bad_words = "$i""
-
-echo "with open("roh.txt") as old_file, open(""$i".txt","w") as newfile:
- for line in old_file:
-  if any(bad_word in line for bad_word in bad_words):
-   newfile.write(line)"
-done
+with open("samples.txt") as fh:
+    for i in fh:
+        i = i.strip()
+        bad_words = i
+        
+        with open("noST_roh.txt") as old_file, open(f"{i}.txt", "w") as newfile:
+            for line in old_file:
+                if bad_words in line:
+                    newfile.write(line)
